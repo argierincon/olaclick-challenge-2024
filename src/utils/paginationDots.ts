ced4dbb6cdbdd92e0c6fa5bef7bb5b347deb33fc
argierincon <argierincon@gmail.com>
2024-12-20 14:53:17 -0500

    feat: table pagination

---
+++ src/utils/paginationDots.ts
@@ -0,0 +1,57 @@
+const DOTS = "...";
+const range = (start: number, end: number) => {
+  let length = end - start + 1;
+  return Array.from({ length }, (_, index) => index + start);
+};
+
+export const getPaginationRange = (
+  totalPageCount: number,
+  currentPage: number
+) => {
+  // Pages count is determined as siblingCount + firstPage + lastPage + currentPage + 2*DOTS
+  const totalPageNumbers = 2;
+
+  /*
+  If the number of pages is less than the page numbers we want to show in our
+  paginationComponent, we return the range [1..totalPageCount]
+  */
+  if (totalPageNumbers >= totalPageCount) {
+    return range(1, totalPageCount);
+  }
+
+  const leftSiblingIndex = Math.max(currentPage - 1, 1);
+  const rightSiblingIndex = Math.min(currentPage + 1, totalPageCount);
+
+  /*
+We do not want to show dots if there is only one position left 
+after/before the left/right page count as that would lead to a change if our Pagination
+component size which we do not want
+*/
+
+  const shouldShowLeftDots = leftSiblingIndex > 2;
+  const shouldShowRightDots = rightSiblingIndex <= totalPageCount - 2;
+
+  const firstPageIndex = 1;
+  const lastPageIndex = totalPageCount;
+
+  if (!shouldShowLeftDots && shouldShowRightDots) {
+    let leftItemCount = 3;
+    let leftRange = range(1, leftItemCount);
+
+    return [...leftRange, DOTS, totalPageCount];
+  }
+
+  if (shouldShowLeftDots && !shouldShowRightDots) {
+    let rightItemCount = 3;
+    let rightRange = range(totalPageCount - rightItemCount + 1, totalPageCount);
+
+    return [firstPageIndex, DOTS, ...rightRange];
+  }
+
+  if (shouldShowLeftDots && shouldShowRightDots) {
+    let middleRange = range(leftSiblingIndex, rightSiblingIndex);
+    return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
+  }
+
+  return range(firstPageIndex, lastPageIndex);
+};
