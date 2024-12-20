f1e6d29ca1b507a2ab45d01c97551adb7297ec80
argierincon <argierincon@gmail.com>
2024-12-20 14:36:51 -0500

    feat: implement initial layout structure

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="layout">
+    <Header />
+    <Sidebar />
+    <main class="content">
+      <router-view v-slot="{ Component }">
+        <transition name="slide" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+
+import Header from "../components/organisms/Header.vue";
+import Sidebar from "../components/organisms/Sidebar.vue";
+
+const route = useRoute();
+const router = useRouter();
+</script>
+
+<style lang="postcss" scoped>
+.layout {
+  display: grid;
+  grid-template-rows: 80px 1fr;
+  grid-template-columns: 300px 1fr;
+  height: 100vh;
+}
+
+.layout > Header {
+  grid-column: span 2;
+  grid-row: 1;
+}
+
+.layout > Sidebar {
+  grid-column: 1;
+  grid-row: 2;
+}
+
+.content {
+  grid-column: 2;
+  grid-row: 2;
+  padding: 1rem;
+  overflow-y: auto;
+}
+
+::v-deep .slide-enter-active,
+::v-deep .slide-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+::v-deep .slide-enter-from {
+  opacity: 0;
+}
+
+::v-deep .slide-leave-to {
+  opacity: 0;
+}
+</style>
