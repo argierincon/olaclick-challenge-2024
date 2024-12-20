018cf1969ac7fd4622eb3ce45e3f0406aadfd840
argierincon <argierincon@gmail.com>
2024-12-20 14:40:51 -0500

    feat: add header component

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <Icon
+      name="MenuBurger"
+      size="24px"
+      class="text-blue-500 hover:text-blue-300"
+    />
+
+    <h3>Órdenes</h3>
+  </header>
+</template>
+
+<script setup lang="ts">
+import Icon from "../atoms/Icon.vue";
+</script>
+
+<style lang="postcss" scoped>
+.header {
+  @apply flex;
+}
+</style>
