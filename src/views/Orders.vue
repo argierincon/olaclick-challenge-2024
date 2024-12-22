64b630621db0d44f142e4bcd9379ba97c2a2dd2b
argierincon <argierincon@gmail.com>
2024-12-22 11:36:29 -0500

    feat: orders data integration first steps

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,8 +1,45 @@
 <template>
-  <h4>{{ prueba }}</h4>
-  <p>Estoy en la pagina de orders</p>
+  <section class="p-3 bg-white rounded-xl">
+    <Table :tableData="data" />
+    <OrderCardsSection />
+  </section>
 </template>
 
 <script setup lang="ts">
-const prueba = "Pagina de ordenes";
+import { onMounted, ref, watch } from "vue";
+import { useGlobalStore } from "../store";
+import type { TStatus } from "../interfaces/Orders";
+
+import OrderCardsSection from "../components/molecules/OrderCardsSection.vue";
+import Table from "../components/organisms/Table.vue";
+import { storeToRefs } from "pinia";
+
+const globalStore = useGlobalStore();
+
+const data = ref();
+const isLoading = ref<boolean>(false);
+
+const getOrdersData = async () => {
+  try {
+    isLoading.value = true;
+    await globalStore.getOrders();
+    data.value = globalStore.ordersData;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+const { tablePage, tableLimit } = storeToRefs(globalStore);
+watch(tableLimit, () => {
+  getOrdersData();
+});
+watch(tablePage, () => {
+  getOrdersData();
+});
+
+onMounted(() => {
+  getOrdersData();
+});
 </script>
