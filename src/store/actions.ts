ed473af1a1077234ca536313abf2a9e0ba5f830d
argierincon <argierincon@gmail.com>
2024-12-22 14:15:06 -0500

    feat: integrate firebase

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,28 +1,80 @@
-import { fetchOrderDetail, fetchOrders } from "../services/apiService";
-import type { IOrder, IOrdersData } from "./interfaces/IOrders";
+import { order } from "../services/apiService";
+import {
+  collection,
+  addDoc,
+  query,
+  getDocs,
+  orderBy,
+  limit,
+  startAfter,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import { db } from "../services/firebase";
+
+import type { IOrder } from "./interfaces/IOrders";
 import type { IState } from "./state";
 
 export const actions = {
-  async getOrders(this: IState) {
-    let orders: IOrdersData;
+  async addOrderToCollection(this: IState) {
     try {
-      const params = { limit: this.tableLimit, page: this.tablePage };
+      console.log(order);
 
-      orders = await fetchOrders(params);
+      const ordersCollection = collection(db, "orders");
+      const docRef = await addDoc(ordersCollection, order);
+      console.log("Order added to Firestore with ID:", docRef.id);
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("Failed to add order to Firestore");
+    }
+  },
+
+  async getOrders(this: IState) {
+    try {
+      const ordersCollection = collection(db, "orders");
+
+      const q = query(
+        ordersCollection,
+        orderBy("id"),
+        limit(this.tableLimit),
+        startAfter(
+          this.tablePage > 1 ? (this.tablePage - 1) * this.tableLimit : 0
+        )
+      );
+
+      const querySnapshot = await getDocs(q);
+
+      const orders: IOrder[] = querySnapshot.docs.map((doc) => ({
+        ...(doc.data() as IOrder),
+        uid: doc.id,
+      }));
+
+      this.ordersData = { data: orders, total: orders.length };
     } catch (error) {
       throw new Error("Failed to fetch orders");
     }
-    this.ordersData = orders;
   },
 
-  async getOrderDetail(this: IState, orderId: number) {
-    let orderDetail: IOrder;
+  async getOrderDetail(this: IState, orderId: string) {
     try {
-      orderDetail = await fetchOrderDetail({ id: orderId });
+      const orderDocRef = doc(db, "orders", orderId);
+
+      const orderDoc = await getDoc(orderDocRef);
+
+      if (orderDoc.exists()) {
+        const orderDetail: IOrder = {
+          ...(orderDoc.data() as IOrder),
+          uid: orderDoc.id,
+        };
+
+        this.orderDetail = orderDetail;
+      } else {
+        throw new Error("Order not found");
+      }
     } catch (error) {
       throw new Error("Failed to fetch order detail");
     }
-    this.orderDetail = orderDetail;
   },
 
   setLimit(this: IState, limit: number) {
