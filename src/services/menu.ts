437510174934677f91f48b208d6ca97f6b336015
argierincon <argierincon@gmail.com>
2024-12-22 17:09:00 -0500

    feat: add generate orders automatically for demo functions

---
+++ src/services/menu.ts
@@ -0,0 +1,302 @@
+export const clients = [
+  { client: "Juan Pérez" },
+  { client: "María González" },
+  { client: "Carlos Sánchez" },
+  { client: "Laura Rodríguez" },
+  { client: "José Martínez" },
+  { client: "Ana López" },
+  { client: "Pedro García" },
+  { client: "Isabel Hernández" },
+  { client: "Luis Fernández" },
+  { client: "Carmen Díaz" },
+  { client: "Antonio Gómez" },
+  { client: "Beatriz Romero" },
+  { client: "Javier Ruiz" },
+  { client: "Teresa Morales" },
+  { client: "Francisco Jiménez" },
+];
+
+const dishes = [
+  {
+    name: "Ceviche",
+    price: 6.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Un plato fresco y cítrico con pescado crudo marinado en jugo de limón y especias.",
+  },
+  {
+    name: "Lomo Saltado",
+    price: 7.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Un delicioso salteado de carne de res, papas y cebollas, acompañado de arroz.",
+  },
+  {
+    name: "Aji de Gallina",
+    price: 6.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Un guiso cremoso y picante de pollo desmenuzado con una salsa de ají amarillo.",
+  },
+  {
+    name: "Pachamanca",
+    price: 8.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Carne, papas y maíz cocinados al estilo tradicional andino, bajo tierra.",
+  },
+  {
+    name: "Arroz con Pollo",
+    price: 5.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Un plato clásico con arroz, pollo y especias, preparado con hierbas frescas.",
+  },
+  {
+    name: "Sancochado",
+    price: 7.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Sopa espesa con carne, vegetales y tubérculos, tradicionalmente servida con arroz.",
+  },
+  {
+    name: "Chicharrón de Cerdo",
+    price: 6.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Crujientes trozos de cerdo fritos, servidos con una salsa o ensalada fresca.",
+  },
+  {
+    name: "Sopa a la Minuta",
+    price: 4.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description: "Una sopa ligera con carne picada, vegetales y arroz.",
+  },
+  {
+    name: "Frejoles con Seco",
+    price: 6.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Frijoles acompañados de carne de res guisada con especias, generalmente servido con arroz.",
+  },
+  {
+    name: "Sudado de Pescado",
+    price: 7.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Plato de pescado cocido al vapor con cebollas, tomates y especias.",
+  },
+  {
+    name: "Tallarín Saltado",
+    price: 6.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Fideos salteados con carne, verduras y salsa de soya, al estilo chino-peruano.",
+  },
+];
+
+const drinks = [
+  {
+    name: "Vaso de Chicha Morada",
+    price: 2.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Jarra de Chicha Morada",
+    price: 7.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Vaso de maracuyá",
+    price: 2.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Jarra de Maracuyá",
+    price: 7.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Vaso de limonada",
+    price: 2.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Jarra de limonada",
+    price: 7.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Vaso de naranjada",
+    price: 2.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Jarra de naranjada",
+    price: 7.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Inca Kola personal",
+    price: 1.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+  {
+    name: "Inca Kola 1.5l",
+
+    price: 4.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+  },
+];
+
+const desserts = [
+  {
+    name: "Picarones",
+    price: 3.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description: "Donas fritas hechas con zapallo, acompañadas de miel.",
+  },
+  {
+    name: "Leche Asada",
+    price: 2.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description:
+      "Postre cremoso de leche, huevo y vainilla, similar a un flan.",
+  },
+  {
+    name: "Mazamorra Morada",
+    price: 2.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description: "Postre a base de maíz morado, azúcar y frutas.",
+  },
+  {
+    name: "Arroz con Leche",
+    price: 2.0,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description: "Arroz cremoso con leche, azúcar y canela.",
+  },
+  {
+    name: "Suspiro a la Limeña",
+    price: 2.5,
+    image:
+      "https://epicwatersgp.com/content/uploads/2020/03/croissant-beef.png",
+    description: "Crema suave de manjar blanco con merengue.",
+  },
+];
+
+let currentOrderId = 1;
+
+const generateOrder = () => {
+  // Generar un ID de orden secuencial
+  const orderId = currentOrderId++;
+
+  // Seleccionar un cliente aleatorio
+  const randomClient = clients[Math.floor(Math.random() * clients.length)];
+
+  // Función para seleccionar un item aleatorio
+  const getRandomItem = (array: any[]) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  };
+
+  // Seleccionar entre 1 y 3 elementos de cada tipo (dishes, drinks, desserts)
+  const getRandomItems = (array: any[], maxItems: number) => {
+    const numItems = Math.floor(Math.random() * maxItems) + 1; // entre 1 y maxItems
+    const items = [];
+
+    for (let i = 0; i < numItems; i++) {
+      const item = getRandomItem(array);
+      const quantity = Math.floor(Math.random() * 3) + 1; // Generar una cantidad aleatoria entre 1 y 3
+
+      // Verificar si el item ya existe en el arreglo, si existe, aumentamos la cantidad
+      const existingItemIndex = items.findIndex((i) => i.name === item.name);
+      if (existingItemIndex !== -1) {
+        items[existingItemIndex].quantity += quantity; // Aumentar la cantidad del item existente
+      } else {
+        items.push({
+          name: item.name,
+          desc: item.description || "", // Si no hay descripción, asignar string vacío
+          price: item.price,
+          quantity,
+          image: item.image,
+        });
+      }
+    }
+
+    return items;
+  };
+
+  // Combina todos los items seleccionados
+  const items = [
+    ...getRandomItems(dishes, 2),
+    ...getRandomItems(drinks, 2),
+    ...getRandomItems(desserts, 2),
+  ];
+
+  // Calcular el subtotal
+  const subtotal = items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  // Calcular el total con impuestos y descuento (en este caso el descuento es 0)
+  const discount = 0;
+  const taxRate = 0.1;
+  const total = subtotal + subtotal * taxRate - discount;
+
+  // Crear la estructura de la orden
+  const order = {
+    id: orderId, // ID único secuencial
+    time: new Date().toISOString(), // Hora actual
+    client: randomClient.client, // Cliente aleatorio
+    status: "started", // Estado inicial
+    items, // Items seleccionados
+    subtotal, // Subtotal
+    total, // Total con impuestos
+    discount, // Descuento
+    taxRate, // Tasa de impuesto
+  };
+
+  return order;
+};
+
+// Se llama entre 1 y 20 veces de manera aleatoria
+export const generateRandomOrders = () => {
+  // Generar un número aleatorio entre 1 y 20
+  const numOrders = Math.floor(Math.random() * 20) + 1;
+
+  const orders = [];
+
+  // Llamar a generateOrder numOrders veces
+  for (let i = 0; i < numOrders; i++) {
+    const order = generateOrder(); // Llamamos a la función para generar una orden
+    orders.push(order); // Añadimos la orden generada al arreglo
+  }
+
+  return orders;
+};
