1f227237bd17e28699167ca880a5882316fa9911
argierincon <argierincon@gmail.com>
2024-12-23 16:16:54 -0500

    feat: add order cards section

---
+++ src/components/molecules/OrderCardsSection.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="order-card-section">
+    <h3 class="text-xl">Órdenes enviadas recientemente</h3>
+    <div class="card-list">
+      <OrderCard
+        v-for="(order, index) in ordersList"
+        :key="index"
+        :client="order.client"
+        :orderId="order.orderId"
+        :status="order.status"
+        :items="order.items"
+        :total="order.total"
+      />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+import OrderCard from "../atoms/OrderCard.vue";
+
+interface IItem {
+  name: string;
+  quantity: number;
+}
+
+interface IOrder {
+  client: string;
+  orderId: string;
+  status: string;
+  items: IItem[];
+  total: number;
+}
+
+interface IProps {
+  ordersList: IOrder[];
+}
+
+defineProps<IProps>();
+</script>
+
+<style lang="postcss" scoped>
+.order-card-section {
+  @apply flex flex-col gap-y-4;
+}
+
+.card-list {
+  @apply flex gap-4;
+  overflow-x: auto;
+  white-space: nowrap;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+.order-card-section::-webkit-scrollbar {
+  display: none;
+}
+
+.order-card {
+  @apply w-56;
+}
+</style>
