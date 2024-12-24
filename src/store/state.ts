4d4f42d51712d9e1d65e9d217bdabf044ac8409f
argierincon <argierincon@gmail.com>
2024-12-24 13:32:20 -0500

    feat: add state to set order updates on real time

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,9 +1,16 @@
 import type { IOrder } from "./interfaces/IOrders";
 
+export interface ILastOrderUpdated {
+  client: string;
+  id: number;
+  status: string;
+}
+
 export interface IState {
   ordersData: IOrder[] | null;
   orderDetail: IOrder | null;
   lastFinishedOrders: IOrder[] | null;
+  lastOrderUpdated: ILastOrderUpdated | null;
   tableTotal: number;
   tableLimit: number;
   tablePage: number;
@@ -13,6 +20,7 @@ export const state = (): IState => ({
   ordersData: [],
   orderDetail: null,
   lastFinishedOrders: null,
+  lastOrderUpdated: null,
   tableTotal: 0,
   tableLimit: 10,
   tablePage: 1,
